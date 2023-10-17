70f7c03eed832290b0bb8bca044fe0ece9673778
samhitag3 <samhita.ghosh@berkeley.edu>
2023-10-17 16:20:12 -0700

    hfkdjsf

--- binning/binner.py
+++ binning/binner.py
@@ -1,4 +1,3 @@
-import pickle
 import os
 import pandas as pd
 
@@ -10,8 +9,10 @@ class Binner():
             self.df = pd.read_pickle(dataFile)
         else:
             self.df = pd.read_csv(dataFile)
-        
+    # def bin(binStyle):
+        # test different binning methods - papers (+ references like qubo), ML based - train on simulation data, train with cone, physics based, occupancy based
     # def binID(self, phi, chi, psi):
     # def binValue(self, binID):
     # def binValue(self, phi, chi, psi):
-    # def plot(self):
+    # def plot2d(self, param):
+    # def plot3d(self):
