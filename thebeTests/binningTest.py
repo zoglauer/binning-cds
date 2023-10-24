e71eae59cd090469807204020d2c610cc69b53be
samhitag3 <samhita.ghosh@berkeley.edu>
2023-10-24 16:36:14 -0700

    dhfkd

--- thebeTests/binningTest.py
+++ thebeTests/binningTest.py
@@ -3,8 +3,8 @@ import os
 import pandas as pd
 import pickle
 
-# sys.path.append(os.path.join(os.path.dirname(sys.path[0]),"binning"))
-# from binner import Binner
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]),"binning"))
+from binner import Binner
 
 # testDataFile = "/volumes/selene/users/andreas/simulationScript/Output/TestSource.999.inc1.id1.tra.gz.pkl"
 # binny = Binner(testDataFile)
