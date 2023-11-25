69bc0922a28cba1004cccb10d4878833a3ddcbf5
jthibault03 <98941496+jthibault03@users.noreply.github.com>
2023-11-24 16:56:23 -0800

    wrappined

--- thebeTests/binningTest.py
+++ thebeTests/binningTest.py
@@ -9,13 +9,16 @@ from binner import Binner
 
 filepath = "/volumes/selene/users/andreas/simulationScript/Output/TestSource.999.inc1.id1.tra.gz.pkl"
 # filepath = "/volumes/selene/users/joseph/simulationScript/simulationResults/FlatContinuumIsotropic.inc381.id1.tra.gz.pkl"
-binny = Binner(filepath)
-# binny.bin()
-# binny.getBinBounds()
-upperbins = binny.bin3D()
+def f():
+    binny = Binner(filepath)
+    # binny.bin()
+    # binny.getBinBounds()
+    upperbins = binny.bin3D()
 
-print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2, "MiB")
-upperbins
+    print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2, "MiB")
+    return upperbins
+
+f()
 # print("Theta Bins")
 # binny.printBins(20, "Theta")
 # print()
