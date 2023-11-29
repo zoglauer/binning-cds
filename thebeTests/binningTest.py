afac04eadfa98a421e655a06c64f78d4cbeaa351
samhitag3 <samhita.ghosh@berkeley.edu>
2023-11-29 15:07:00 -0800

    test num

--- thebeTests/binningTest.py
+++ thebeTests/binningTest.py
@@ -7,7 +7,7 @@ import psutil
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]),"binning"))
 from binner import Binner
 
-filepath = "/volumes/selene/users/andreas/simulationScript/Output/TestSource.999.inc1.id1.tra.gz.pkl"
+filepath = "/volumes/selene/users/andreas/simulationScript/Output/TestSource.726.inc1.id1.tra.gz.pkl"
 # filepath = "/volumes/selene/users/joseph/simulationScript/simulationResults/FlatContinuumIsotropic.inc381.id1.tra.gz.pkl"
 
 binny = Binner(filepath)
